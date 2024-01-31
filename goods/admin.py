68ea932f27db22b4bbb7d8659da226df84f166ac
onlinestore <example@mail.com>
2024-01-31 22:30:29 +0700

    Commit 4 After adding fixtures for goods

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from goods.models import Categories, Products
+
+# admin.site.register(Categories)
+# admin.site.register(Products)
+
+@admin.register(Categories)  # регистрируем класс в адимнистраторе
+class CategoriesAdmin(admin.ModelAdmin):
+    #  производим тонкую настройку полей таблиц в адинпанели
+    prepopulated_fields = {'slug': ('name',)}  # делаем заполнение поля URL в таблице Категории автоматически
+
+@admin.register(Products)  # регистрируем класс в адимнистраторе
+class ProductsAdmin(admin.ModelAdmin):
+    #  производим тонкую настройку полей таблиц в адинпанели
+    prepopulated_fields = {'slug': ('name',)}  # делаем заполнение поля URL в таблице Продукты автоматически
+
+
--- main/views.py
+++ main/views.py
@@ -2,11 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import context
 
+# Получаем доступ к ОРМ Системе а именно к Категориям
+from goods.models import Categories 
+
+
 # обработчик запросов (контроллеров) на главной странице сайта
 def index(request):
+    
+    categories = Categories.objects.all()
     context = {
         'title': 'VIA - Главная',
-        'content': 'Магазин мебели VIA'               
+        'content': 'Магазин мебели в стиле "Ретро" VIA',
+        'categories': categories               
     }
     
     return render(request, 'main/index.html', context) # главная страница
