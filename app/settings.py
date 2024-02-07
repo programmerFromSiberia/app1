e662a04ff199185b1156ac74d7baa7d02805a22e
programmerFromSiberia <valkevicholeg@mail.ru>
2024-02-07 22:39:53 +0700

    PostgreSQL data base connected & full text search

--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     
     "debug_toolbar", # регистрируем приложение "debug_toolbar
     
@@ -83,8 +84,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'via',
+        'USER': 'via',
+        'PASSWORD': 'via',
+        'HOST': 'localhost',
+        'PORT': '5432',  
     }
 }
 
--- goods/utils.py
+++ goods/utils.py
@@ -2,18 +2,27 @@ from django.db.models import Q
 
 from goods.models import Products
 
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 def q_search(query):  # поиск по поисковому запросу
     
-    if query.isdigit() and len(query) <= 5: # если поисковый запрос состоит из цифр и длина меньше 5 символов
+    if query.isdigit() and len(query) <= 5: # если поисковый запрос состоит из цифр и длина меньше или = 5 символов
        return Products.objects.filter(id=int(query))  # отображаем продукт по id
-   
-    keywords = [word for word in query.split() if len(word) > 2]  # список ключевых слов
-    q_objects = Q()
     
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)  # поиск по ключевым словам в описании товара
-        q_objects |= Q(name__icontains=token)  # поиск по ключевым словам в названии товара
+    # поисковик Джанго из коробки https://docs.djangoproject.com/en/5.0/ref/contrib/postgres/search/
+    
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
+    
+    return Products.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by("-rank")  
+      
+    
+    # keywords = [word for word in query.split() if len(word) > 2]  # список ключевых слов
+    # q_objects = Q()
+    
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)  # поиск по ключевым словам в описании товара
+    #     q_objects |= Q(name__icontains=token)  # поиск по ключевым словам в названии товара
         
-    return Products.objects.filter(q_objects)
+    # return Products.objects.filter(q_objects)
         
