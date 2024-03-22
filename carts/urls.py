de85315a7d9b9fe03a1d756672964c17274d7eb4
programmerFromSiberia <valkevicholeg@mail.ru>
2024-03-23 00:31:16 +0700

    Add to cart view, refactor included cart templates

--- carts/urls.py
+++ carts/urls.py
@@ -7,7 +7,7 @@ app_name = "carts"  # пространство имен
 # https://docs.djangoproject.com/en/5.0/topics/http/urls/ - пример конфигурации
 
 urlpatterns = [
-    path('cart_add/<int:product_id>/', views.cart_add, name='cart_add'), # добавляем товар в корзину
-    path('cart_change/<int:product_id>/', views.cart_change, name='cart_change'), # изменяем кол-во товара в корзине
-    path('cart_remove/<int:product_id>/', views.cart_remove, name='cart_remove'), # удаляем товар из корзины
+    path('cart_add/<slug:product_slug>/', views.cart_add, name='cart_add'), # добавляем товар в корзину
+    path('cart_change/<slug:product_slug>/', views.cart_change, name='cart_change'), # изменяем кол-во товара в корзине
+    path('cart_remove/<slug:product_slug>/', views.cart_remove, name='cart_remove'), # удаляем товар из корзины
 ]
